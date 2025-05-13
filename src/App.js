97c93f287f7bf92a443c45768cf82f944b3fdd34
Pavithran Ambur Venkatesan <support@eastghats.com>
2025-05-13 18:52:25 +0530

    smaple react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Hello from Jenkins CI/CD!</h1>
+      <p>This React app was built and deployed by Jenkins 🚀</p>
     </div>
   );
 }
